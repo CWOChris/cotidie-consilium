dbe518b5e030c961b1fb754c3fd4c1b52d55fcb2
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-10-04 00:35:24 -0500

    this doesn't work and also makes my save icon disappear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 // I need to make a function that will change the class based on the current time of day
+
+
+
+
+
+
 // I need to make a function that will change the background image based on the current time of day
+
+
+
 // I need to make a function that will change the text color based on the current time of day
 
-var currentDay = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(currentDay);
+
+
+
+// I need to save the contents of a text area to local storage when the save button is clicked
+var image = document.querySelector("img");
+var input = document.querySelector("input");
+
+
+image.addEventListener("click", function() => {
+    localStorage.setItem("name", input.value);
+    image.src = input.value;
+    });
