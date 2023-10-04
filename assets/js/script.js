c8df8eebae9806ffa319155f82ca204d0d8477c5
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-10-04 01:26:25 -0500

    added the local storage javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,112 @@
 
 
 // I need to save the contents of a text area to local storage when the save button is clicked
-var button = document.querySelector("button");
-var input = document.querySelector("input");
-button.addEventListener("click", () => {
-    localStorage.setItem("0900", input.value);
+
+// 0700
+var textArea = document.getElementById("textArea");
+var saveButton = document.getElementById("saveButton");
+
+saveButton.addEventListener("click", function() {
+    var textSaved = textArea.value;
+    localStorage.setItem("0700", textSaved);
 });
 
-console.log(button);
-console.log(input);
+//0800
+var textArea1 = document.getElementById("textArea1");
+var saveButton1 = document.getElementById("saveButton1");
+
+saveButton1.addEventListener("click", function() {
+    var textSaved1 = textArea1.value;
+    localStorage.setItem("0800", textSaved1);
+});
+
+//0900
+var textArea2 = document.getElementById("textArea2");
+var saveButton2 = document.getElementById("saveButton2");
+
+saveButton2.addEventListener("click", function() {
+    var textSaved2 = textArea2.value;
+    localStorage.setItem("0900", textSaved2);
+});
+
+//1000
+var textArea3 = document.getElementById("textArea3");
+var saveButton3 = document.getElementById("saveButton3");
+
+saveButton3.addEventListener("click", function() {
+    var textSaved3 = textArea3.value;
+    localStorage.setItem("1000", textSaved3);
+});
+
+//1100
+var textArea4 = document.getElementById("textArea4");
+var saveButton4 = document.getElementById("saveButton4");
+
+saveButton4.addEventListener("click", function() {
+    var textSaved4 = textArea4.value;
+    localStorage.setItem("1100", textSaved4);
+});
+
+//1200
+var textArea5 = document.getElementById("textArea5");
+var saveButton5 = document.getElementById("saveButton5");
+
+saveButton5.addEventListener("click", function() {
+    var textSaved5 = textArea5.value;
+    localStorage.setItem("1200", textSaved5);
+});
+
+//1300
+var textArea6 = document.getElementById("textArea6");
+var saveButton6 = document.getElementById("saveButton6");
+
+saveButton6.addEventListener("click", function() {
+    var textSaved6 = textArea6.value;
+    localStorage.setItem("1300", textSaved6);
+});
+
+//1400
+var textArea7 = document.getElementById("textArea7");
+var saveButton7 = document.getElementById("saveButton7");
+
+saveButton7.addEventListener("click", function() {
+    var textSaved7 = textArea7.value;
+    localStorage.setItem("1400", textSaved7);
+});
+
+//1500
+var textArea8 = document.getElementById("textArea8");
+var saveButton8 = document.getElementById("saveButton8");
+
+saveButton8.addEventListener("click", function() {
+    var textSaved8 = textArea8.value;
+    localStorage.setItem("1500", textSaved8);
+});
+
+//1600
+var textArea9 = document.getElementById("textArea9");
+var saveButton9 = document.getElementById("saveButton9");
+
+saveButton9.addEventListener("click", function() {
+    var textSaved9 = textArea9.value;
+    localStorage.setItem("1600", textSaved9);
+});
+
+//1700
+var textArea10 = document.getElementById("textArea10");
+var saveButton10 = document.getElementById("saveButton10");
+
+saveButton10.addEventListener("click", function() {
+    var textSaved10 = textArea10.value;
+    localStorage.setItem("1700", textSaved10);
+});
+
+//1800
+var textArea11 = document.getElementById("textArea11");
+var saveButton11 = document.getElementById("saveButton11");
+
+saveButton11.addEventListener("click", function() {
+    var textSaved11 = textArea11.value;
+    localStorage.setItem("1800", textSaved11);
+});
+
